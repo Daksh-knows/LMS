258af10be346402389af7635eda5f2d863855f48
Pranav Nair <nairpranav31@gmail.com>
2026-02-17 17:02:37 +0530

    Testing dynamic rendering of apis

--- app/api/admin/assignments/[lectureId]/route.ts
+++ app/api/admin/assignments/[lectureId]/route.ts
@@ -1,7 +1,7 @@
 import { NextRequest, NextResponse } from "next/server";
 import { db } from "@/lib/db";
 import { auth } from "@/auth";
-
+export const dynamic = "force-dynamic";
 export async function GET(
   req: NextRequest,
   context: { params: Promise<{ lectureId: string }> }
