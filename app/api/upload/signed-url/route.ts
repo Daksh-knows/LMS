bdcdad924fc07b1bdf4f8376d44441ab7161c87d
Pranav Nair <nairpranav31@gmail.com>
2026-02-25 15:09:30 +0530

    commented reserve api

--- app/api/upload/signed-url/route.ts
+++ app/api/upload/signed-url/route.ts
@@ -1,34 +1,36 @@
 import { NextResponse } from "next/server";
-import { storage, bucketName, parsePrivateKey } from "@/lib/google-cloud";
+// import { storage, bucketName, parsePrivateKey } from "@/lib/google-cloud";
 
 export async function POST(req: Request) {
   try {
-    const { fileName, contentType } = await req.json();
-    console.log('****************************************')
-    const parsed = parsePrivateKey(process.env.GCS_PRIVATE_KEY);
-    console.log("Parsed key first 80 chars:", parsed?.substring(0, 80));
-    console.log("Has real newline:", parsed?.includes('\n'));
-    console.log('****************************************')
+    console.log("Hi");
+  
+  //   const { fileName, contentType } = await req.json();
+  //   console.log('****************************************')
+  //   const parsed = parsePrivateKey(process.env.GCS_PRIVATE_KEY);
+  //   console.log("Parsed key first 80 chars:", parsed?.substring(0, 80));
+  //   console.log("Has real newline:", parsed?.includes('\n'));
+  //   console.log('****************************************')
 
-    // Create a unique filename for the bucket
-    const uniqueFilename = `course-videos/${Date.now()}-${fileName.replace(/\s/g, "_")}`;
+  //   // Create a unique filename for the bucket
+  //   const uniqueFilename = `course-videos/${Date.now()}-${fileName.replace(/\s/g, "_")}`;
 
-    // Generate a Signed URL that expires in 15 minutes
-    // This URL allows a specific "PUT" operation for this specific file
-    const [url] = await storage
-      .bucket(bucketName)
-      .file(uniqueFilename)
-      .getSignedUrl({
-        version: "v4",
-        action: "write",
-        expires: Date.now() + 15 * 60 * 1000,
-        contentType: contentType,
-      });
+  //   // Generate a Signed URL that expires in 15 minutes
+  //   // This URL allows a specific "PUT" operation for this specific file
+  //   const [url] = await storage
+  //     .bucket(bucketName)
+  //     .file(uniqueFilename)
+  //     .getSignedUrl({
+  //       version: "v4",
+  //       action: "write",
+  //       expires: Date.now() + 15 * 60 * 1000,
+  //       contentType: contentType,
+  //     });
 
-    return NextResponse.json({
-      url: url, // The secret temporary URL to upload to
-      publicUrl: `https://storage.googleapis.com/${bucketName}/${uniqueFilename}` // The permanent URL to save in DB
-    });
+  //   return NextResponse.json({
+  //     url: url, // The secret temporary URL to upload to
+  //     publicUrl: `https://storage.googleapis.com/${bucketName}/${uniqueFilename}` // The permanent URL to save in DB
+  //   });
 
   } catch (error) {
     console.error("Signed URL Error:", error);
